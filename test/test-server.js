6cd448378e93dbc9417a8c5d29a3b760d5617d9c
Ryan Lynn <ryanlynn523@gmail.com>
2016-08-05 13:49:08 -0400

    added 2 more tests for dashboard and search results page

--- test/test-server.js
+++ test/test-server.js
@@ -6,12 +6,29 @@ var app = server.app;
 
 chai.use(chaiHttp);
 
-describe('name', function() {
+describe('Shelter Dog App', function() {
     it('server is on!', function() {
         chai.request(app)
             .get('/')
             .end(function(err, res) {
                 expect(res).to.have.status(200);
+                expect(res).to.be.html;
             });
+    });
+    it('connect to dashboard page',function() {
+        chai.request(app)
+        .get('/dashboard.html')
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
+    });
+    it('connect to search results page',function() {
+        chai.request(app)
+        .get('/search-results.html')
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res).to.be.html;
+        });
     });
 });
