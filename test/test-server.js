139907339abd1fb85ec3e42b8dafaef72036476e
Ryan Lynn <ryanlynn523@gmail.com>
2016-08-05 16:28:11 -0400

    refactored mocha/chai tests

--- test/test-server.js
+++ test/test-server.js
@@ -2,33 +2,37 @@ var chai = require('chai');
 var mocha = require('mocha');
 var chaiHttp = require('chai-http');
 var server = require('../server');
+var should = chai.should();
 var app = server.app;
 
 chai.use(chaiHttp);
 
 describe('Shelter Dog App', function() {
-    it('server is on!', function() {
+    it('server is on!', function(done) {
         chai.request(app)
             .get('/')
             .end(function(err, res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.html;
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
             });
     });
-    it('connect to dashboard page',function() {
+    it('connect to dashboard page',function(done) {
         chai.request(app)
         .get('/dashboard.html')
         .end(function(err, res) {
-            expect(res).to.have.status(200);
-            expect(res).to.be.html;
+            res.should.have.status(200);
+            res.should.be.html;
+            done();
         });
     });
-    it('connect to search results page',function() {
+    it('connect to search results page',function(done) {
         chai.request(app)
         .get('/search-results.html')
         .end(function(err, res) {
-            expect(res).to.have.status(200);
-            expect(res).to.be.html;
+            res.should.have.status(200);
+            res.should.be.html;
+            done();
         });
     });
 });
